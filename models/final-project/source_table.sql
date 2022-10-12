6fe4b010b9f43305a397f128375b535868b1c8bf
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2022-10-12 11:38:30 +0700

    Add files via upload

--- models/final-project/source_table.sql
+++ models/final-project/source_table.sql
@@ -1,1 +1,8 @@
-select * from {{source('fraud_dataset_timestamp','fraud_dataset_timestamp')}}
+{{
+    config(
+        materialized="view"
+    )
+}}
+
+SELECT * FROM `data-fellowship-7-363107.final_project.final_project_with_timestamp`
+ORDER BY timestamp, step
